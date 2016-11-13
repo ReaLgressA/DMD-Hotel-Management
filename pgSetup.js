3e6cc4c485a3ecae4c7668388822fad4c9feb1cf
Луговской Владислав <lugovskoy.v@gmail.com>
2016-11-13 12:56:50 +0300

    Hotels create and index

--- pgSetup.js
+++ pgSetup.js
@@ -1,6 +1,6 @@
 var query = require("pg-query");
 
-var conString = "pg://postgres:3081331304@localhost:5432/postgres";
+var conString = "pg://postgres:3081331304@localhost:5432/hotel";
 query.connectionParameters = conString;
 
 module.exports = query;
--- routes/manage.js
+++ routes/manage.js
@@ -18,9 +18,44 @@ router.get('/hotels', function(req, res, next) {
         if(err) {
             console.error(err);
         }
-        res.render('manage', { title: 'Hotel management', data: rows, column_names: result.fields });
+        res.render('manage/hotels', { title: 'Hotel management', data: rows, column_names: ['Id', 'Name', 'Description', '     Stars', 'Country']});
     });
 });
+router.get('/hotels/create', function(req, res) {
+    query('SELECT * FROM public."Hotel"', function(err, rows, result) {
+        if(err) {
+            console.error(err);
+        }
+        res.render('manage/hotels_create', { column_names: result.fields });
+    });
+});
+router.post('/hotels/create', function (req, res) {
+    var post = {
+        name: req.body['name'],
+        description: req.body['desc'],
+        stars: req.body['rating'],
+        country_code: req.body['country']
+    };
+    var postArray = [req.body['name'], req.body['desc'], req.body['rating'], req.body['country']];
+    console.log(post);
+    query('INSERT INTO public."Hotel" (name, description, stars, country_code) VALUES($1, $2, $3, $4)', postArray, function(err, rows, result) {
+        if(err) {
+            console.error(err);
+        } else {
+            res.redirect('/manage/hotels');
+        }
+    });
+    // sql.query('INSERT INTO GadgetType SET ?', post, function (err, results, fields) {
+    //     if(err == null) {
+    //         res.redirect('/gadgets/types');
+    //     } else {
+    //         res.render('status', { status: "Not created error: " + err });
+    //     }
+    // });
+});
+
+
+
 
 router.get('/rooms', function(req, res, next) {
     query('SELECT * FROM public."Room"', function(err, rows, result) {
@@ -55,7 +90,7 @@ router.get('/discounts', function(req, res, next) {
         res.render('manage', { title: 'Discount management', data: rows, column_names: result.fields });
     });
 });
-router.get('/Reservations', function(req, res, next) {
+router.get('/reservations', function(req, res, next) {
     query('SELECT * FROM public."Reservation"', function(err, rows, result) {
         if(err) {
             console.error(err);
@@ -63,7 +98,7 @@ router.get('/Reservations', function(req, res, next) {
         res.render('manage', { title: 'Reservation management', data: rows, column_names: result.fields });
     });
 });
-router.get('/Clients', function(req, res, next) {
+router.get('/clients', function(req, res, next) {
     query('SELECT * FROM public."Client"', function(err, rows, result) {
         if(err) {
             console.error(err);
@@ -71,12 +106,20 @@ router.get('/Clients', function(req, res, next) {
         res.render('manage', { title: 'Client management', data: rows, column_names: result.fields });
     });
 });
-router.get('/Users', function(req, res, next) {
+router.get('/users', function(req, res, next) {
     query('SELECT * FROM public."User"', function(err, rows, result) {
         if(err) {
             console.error(err);
         }
         res.render('manage', { title: 'User management', data: rows, column_names: result.fields });
+    });
+});
+router.get('/countries', function(req, res, next) {
+    query('SELECT * FROM public."Country"', function(err, rows, result) {
+        if(err) {
+            console.error(err);
+        }
+        res.render('manage', { title: 'Country management', data: rows, column_names: result.fields });
     });
 });
 module.exports = router;
